6ff5f6523f9dbbf9bb6b2317d2eca3735a2de525
PavelBfl <33580861+PavelBfl@users.noreply.github.com>
2019-12-17 23:58:28 +0500

    + Реализован генератор дробных значений

--- ValueGenerator/Int32Generators/Int32Generator.cs
+++ ValueGenerator/Int32Generators/Int32Generator.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ValueGenerator.Int32Generators
 {
-	public class Int32Generator : ValueGenerator, IValueGenerator<int>, IValuesUniqueGenerator<int>
+	public class Int32Generator : ValueGenerator, IValueGenerator<int>
 	{
 		public Int32Generator(Random random)
 			: base(random)
@@ -14,26 +14,6 @@ namespace ValueGenerator.Int32Generators
 		public int Generate()
 		{
 			return Random.Next();
-		}
-
-		private readonly SortedSet<int> oldValues = new SortedSet<int>();
-		public int GenerateUnique()
-		{
-			var result = Generate();
-			foreach (var oldValue in oldValues)
-			{
-				if (oldValue == result)
-				{
-					result++;
-				}
-			}
-			oldValues.Add(result);
-			return result;
-		}
-
-		public void Reset()
-		{
-			oldValues.Clear();
 		}
 	}
 }
